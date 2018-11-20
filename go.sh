b971b58e54280dccb5a34a2b8f6ee7c36b876b25
Grant Heffernan <heffergm@gmail.com>
2018-11-20 10:50:57 -0500

    go.sh

--- go.sh
+++ go.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-count=40
-nap=1
-ports="8547"
-hostname="hostname.something.com"
+count=10 # runs 10 iterations resulting in 20 total clients: 10 streamers and 10 players
+nap=1 # sleep time between iterations
+ports="8547 8548" # list of ports to connect to
+hostname="hostname.something.com" 
 
 while [[ ${count} -gt 0 ]]; do
   random=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
@@ -16,6 +16,7 @@ while [[ ${count} -gt 0 ]]; do
     sudo docker run -d heffergm/rtsp /app/rtsp-client-player.sh --rtsp-url=rtsp://$hostname:$port/$random --video=true --audio=false
    done
 
+  let "count-=1"
   sleep ${nap}
 
   echo "Running clients: $(sudo docker ps | grep client | wc -l)"
